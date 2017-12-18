5357424952319e234f5c69e7c1fafef5d5f6af08
Anna Thomas <antho@microsoft.com>
2017-12-18 17:50:35 -0500

    redoing 1.1 draft 0

---
+++ lab01.1-computer_vision/resources/code/Finished-ImageProcessing - Computer Vision/ProcessingLibrary/ImageProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Vision;
+using ServiceHelpers;
+
+namespace ProcessingLibrary
+{
+    public class ImageProcessor
+    {
+       public static async Task<ImageInsights> ProcessImageAsync(Func<Task<Stream>> imageStreamCallback, string imageId)
+        {
+            VisualFeature[] DefaultVisualFeaturesList = new VisualFeature[] { VisualFeature.Tags, VisualFeature.Description };
+
+            var imageAnalysisResult = await VisionServiceHelper.AnalyzeImageAsync(imageStreamCallback, DefaultVisualFeaturesList);
+
+            ImageInsights result = new ImageInsights
+            {
+                ImageId = imageId,
+                Caption = imageAnalysisResult.Description.Captions[0].Text,
+                Tags = imageAnalysisResult.Tags.Select(t => t.Name).ToArray()
+            };
+
+            return result;
+        }
+
+    }
+
+}
