d673cdf7101d10b306f9ea851085b907720757d0
Anna Thomas <antho@microsoft.com>
2018-04-02 08:03:51 -0700

    added v3 bot labs back, moved v4 to folder, added note

--- lab02.2-building_bots/building_bots-NEW_Bot_Framework/resources/code/FinishedPictureBot-Part4/PictureBot/Models/SearchHit.cs
+++ lab02.2-building_bots/building_bots-NEW_Bot_Framework/resources/code/FinishedPictureBot-Part4/PictureBot/Models/SearchHit.cs
@@ -3,22 +3,16 @@ using System.Collections.Generic;
 
 namespace PictureBot.Models
 {
-    [Serializable]
     public class SearchHit
     {
         public SearchHit()
         {
             this.PropertyBag = new Dictionary<string, object>();
         }
-
         public string Key { get; set; }
-
         public string Title { get; set; }
-
         public string PictureUrl { get; set; }
-
         public string Description { get; set; }
-
         public IDictionary<string, object> PropertyBag { get; set; }
     }
 }
